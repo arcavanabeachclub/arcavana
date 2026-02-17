7c9efe071aea8860f8539289bab1260e6f90723e
arcavanabeachclub <avnadam1997@gmail.com>
2026-02-17 14:50:12 +0700

    Create webhook.js

---
+++ api/sociabuzz/api/sociabuzz/webhook.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.status(200).json({ success: true });
+  }
+
+  res.status(405).json({ message: "Method not allowed" });
+}
