2c5ef00c42f43eea7d9d17303cc4cb38bb9deb70
arcavanabeachclub <avnadam1997@gmail.com>
2026-02-17 15:16:26 +0700

    Update get-donations.js

--- api/sociabuzz/get-donations.js
+++ api/sociabuzz/get-donations.js
@@ -1,6 +1,15 @@
+let donations = global.donations || [];
+global.donations = donations;
+
 export default function handler(req, res) {
+
+  const dataToSend = [...donations];
+
+  // kosongkan setelah dikirim
+  donations.length = 0;
+
   res.status(200).json({
     success: true,
-    donations: []
+    donations: dataToSend
   });
 }
